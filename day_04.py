b5388d254f444ce950cf3631c3beca82d00c938f
Anders Eriksson <eriksson.c.anders@gmail.com>
2022-12-16 22:36:18 +0100

    solve day 4

--- day_04.py
+++ day_04.py
@@ -4,21 +4,22 @@ from sys import stdin
 # Day 04
 
 def star1(section_ids):
-    for a, b, c, d in section_ids:
-        print(a, "\t", b, "\t", c, "\t", d, "\t", fully_contains(a, b, c, d))
     return sum([fully_contains(*pair) for pair in section_ids])
 
 def star2(section_ids):
-    return 0
+    return sum([overlaps(*pair) for pair in section_ids])
 
 def fully_contains(a, b, c, d):
     return (a <= c and b >= d) or (c <= a and d >= b)
+
+def overlaps(a, b, c, d):
+    return b >= c and d >= a
 
 ### Input etc.
 
 def parse_line(line):
     pattern = r"(\d+)-(\d+).(\d+)-(\d+)"
-    return re.match(pattern, line).groups() # type: ignore
+    return [int(x) for x in re.match(pattern, line).groups()] # type: ignore
 
 section_ids = [parse_line(line.rstrip()) for line in stdin.readlines()]
 print(f"03_1: {star1(section_ids)}")
